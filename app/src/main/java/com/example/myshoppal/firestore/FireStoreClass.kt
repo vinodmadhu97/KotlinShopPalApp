cad9fc5884fb23ca13724c64e4fb8704b87b206d
vinodmadhu97 <vinodmadhu97@gmail.com>
2021-08-08 11:20:24 +0530

    firestore implemented

---
+++ app/src/main/java/com/example/myshoppal/firestore/FireStoreClass.kt
@@ -0,0 +1,8 @@
+package com.example.myshoppal.firestore
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+
+class FireStoreClass {
+    private val mFireStore = FirebaseFirestore.getInstance()
+}
---
+++ app/src/main/java/com/example/myshoppal/models/User.kt
@@ -0,0 +1,4 @@
+package com.example.myshoppal.models
+
+class User {
+}
