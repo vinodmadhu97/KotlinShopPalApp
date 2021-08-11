1c1451c2f2d9d651a1a5b31fb3cdb47022e47f02
vinodmadhu97 <vinodmadhu97@gmail.com>
2021-08-11 21:17:55 +0530

    permission handling(manualy)

--- app/src/main/java/com/example/myshoppal/activities/UserProfileActivity.kt
+++ app/src/main/java/com/example/myshoppal/activities/UserProfileActivity.kt
@@ -1,16 +1,26 @@
 package com.example.myshoppal.activities
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.myshoppal.R
 import com.example.myshoppal.models.User
 import com.example.myshoppal.utils.Constants
 import kotlinx.android.synthetic.main.activity_user_profile.*
+import java.util.jar.Manifest
 
-class UserProfileActivity : AppCompatActivity() {
+class UserProfileActivity : BaseActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+
+        hideStatusBar()
 
         var user = User()
 
@@ -26,6 +36,60 @@ class UserProfileActivity : AppCompatActivity() {
 
         et_profile_Email.isEnabled = false
         et_profile_Email.setText(user.email)
+
+        iv_profile_pic.setOnClickListener(this)
+
+    }
+
+    private fun hideStatusBar(){
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null){
+            when(v.id){
+                R.id.iv_profile_pic ->{
+                    checkPermissions()
+                }
+            }
+        }
+    }
+
+    private fun checkPermissions(){
+        if (ContextCompat.checkSelfPermission(
+                this,android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            showSnackBar("already permission granted",false)
+        }else{
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                Constants.READ_STORAGE_PERMISSION_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED ){
+                showSnackBar("storage permission granted",false)
+            }else{
+                showSnackBar("storage permission denited",true)
+            }
+        }
 
     }
 }
